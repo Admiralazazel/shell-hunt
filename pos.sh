d8e76a8eb23a85f3a4f90741a147b59391c206fb
Aaditya Chapagain <admiral.azazel@gmail.com>
2019-04-14 18:16:54 +0545

    Positional Arguments updated

--- pos.sh
+++ pos.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -gt 0 ]; then
+    echo "Your command line contains $# arguments"
+else
+    echo "Your command line contains no arguments"
+fi
+
 echo "Positional Parameters"
 echo '$0 = ' $0
 echo '$1 = ' $1
